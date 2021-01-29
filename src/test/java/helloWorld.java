2aa1510968ad9838cbd274854973dab1fa82341d
lcsi3637 <64227008+lcsi3637@users.noreply.github.com>
2021-01-28 23:42:25 -0500

    Update helloWorld.java
    herseyi sildim abi

--- src/test/java/helloWorld.java
+++ src/test/java/helloWorld.java
@@ -1,11 +1,7 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Merhaba");
+        
 
-       System.out.println("Saban's sout2");
-
-        System.out.println("Hos geldin Mehmet hocam");
-        System.out.println("baska bisi hocam");
 
     }
 
