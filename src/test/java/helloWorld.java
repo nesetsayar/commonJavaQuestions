fb77159be8f63a83b64c2e162887181872ba81b3
lcsi3637 <64227008+lcsi3637@users.noreply.github.com>
2021-01-28 23:37:45 -0500

    Update helloWorld.java
    tekrar degistim bilader

--- src/test/java/helloWorld.java
+++ src/test/java/helloWorld.java
@@ -2,9 +2,7 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Merhaba");
 
-System.out.println("Mehmet hocam geldi");
-        
-        System.out.println("Saban's sout2");
+       System.out.println("Saban's sout2");
 
     }
 
